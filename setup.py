2c696d7182bf6f857842e2ae95efa5eaa4fb2594
Peith Vergil <peith.vergil@gmail.com>
2015-12-13 14:26:19 +0800

    Add more classifiers. Check if Cython compilation is available or not.

--- setup.py
+++ setup.py
@@ -2,10 +2,28 @@ from distutils.core import Extension, setup
 
 from Cython.Build import cythonize
 
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
 
-extensions = [
-    Extension('*', ['mathix/*.pyx']),
-]
+
+if use_cython:
+    extensions = [
+        Extension('mathix.vector', ['mathix/vector.pyx']),
+    ]
+
+    cmdclass = {
+        'build_ext': build_ext
+    }
+else:
+    extensions = [
+        Extension('mathix.vector', ['mathix/vector.c']),
+    ]
+
+    cmdclass = {}
 
 
 setup(
@@ -14,6 +32,26 @@ setup(
     author='Peith Vergil',
 
     version='0.1',
+
+    cmdclass=cmdclass,
+
+    packages=[
+        'mathix',
+    ],
+
+    keywords='useless simple math library',
+
+    description='A useless simple math library.',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Cython',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 
     ext_modules=cythonize(extensions)
 )
